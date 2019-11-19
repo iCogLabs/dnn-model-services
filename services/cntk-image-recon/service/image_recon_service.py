839cc4540e4d9a0510cac45e5f8f57409ba981ad
yitbarekyimam <41948133+yitbarek123@users.noreply.github.com>
2019-11-19 14:16:40 +0300

    Update image_recon_service.py

--- services/cntk-image-recon/service/image_recon_service.py
+++ services/cntk-image-recon/service/image_recon_service.py
@@ -71,7 +71,9 @@ class RecognizerServicer(grpc_bt_grpc.RecognizerServicer):
 #
 # Add all your classes to the server here.
 def serve(max_workers=10, port=7777):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
+    grpc_msg_size = (1024 ** 2) * 100   # max grpc message size
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers), options=[
+            ('grpc.max_receive_message_length', grpc_msg_size))
     grpc_bt_grpc.add_RecognizerServicer_to_server(RecognizerServicer(), server)
     server.add_insecure_port("[::]:{}".format(port))
     return server
